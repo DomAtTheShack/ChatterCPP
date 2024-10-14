94f7ff93ea27768bca4faccaf8c6d51665bb9e1f
dominichann <dominic@domsmacshack.com>
2024-10-14 13:05:19 -0400

    Added Documentation to the methods within the main.cpp of the server

--- Packet.cpp
+++ Packet.cpp
@@ -18,6 +18,9 @@
 
 using namespace std;
 
+/**
+ *  This initializes the packet with default values that are never to be seen
+ */
 Packet::Packet()
 {
     usr = "SERVER_DEBUG";
@@ -25,6 +28,10 @@ Packet::Packet()
     message = "REALLY YOU SHOULD WATCH BLUEY";
 }
 
+/**
+ * Creates a packet with just a user and this main use is for a initial connection packet on first communication
+ * @param usr The username to be stored in this packet
+ */
 Packet::Packet(std::string usr)
 {
     this->usr = std::move(usr);
@@ -32,17 +39,29 @@ Packet::Packet(std::string usr)
     message = "$CONNECT_PKT";
 }
 
-
+/**
+ * The main packet to sent with the username the message and user ID
+ */
 Packet::Packet(std::string usr, string msg)
 {
     this->id = generateUSERID();
     this->usr = std::move(usr);
     message = std::move(msg);
 }
+
+/**
+ * Default deconstruct
+ */
 Packet::~Packet() = default;
 
-// Serialize the object into a byte buffer
-void Packet::serialize(char* buffer, size_t size, Packet* pkt) {
+/** Serialize the object into a byte buffer to be sent over a socket
+ *
+ * @param buffer the byte buffer array to be stored with the packet data
+ * @param size the size of the packet itself to be sent
+ * @param pkt the packet object to be serialized
+ */
+void Packet::serialize(char* buffer, size_t size, Packet* pkt)
+{
     // Ensure the buffer size is sufficient
     if (size < pkt->getSerializedSize()) return;
 
@@ -64,12 +83,22 @@ void Packet::serialize(char* buffer, size_t size, Packet* pkt) {
     serializeString(buffer, size, offset, pkt->message);
 }
 
-void Packet::serializeString(char* buffer, size_t bufferSize, size_t& offset, const std::string& stringToSerial) {
+/**
+ * This method will serialize the string its given into byte data
+ *
+ * @param buffer the buffer array to be stored with the byte data of the string
+ * @param bufferSize the size of the buffer to be checking
+ * @param offset the offset of the array or the i value of the array index
+ * @param stringToSerial the string itself to be serialized
+ */
+void Packet::serializeString(char* buffer, size_t bufferSize, size_t& offset, const std::string& stringToSerial)
+{
     // Determine the length of the string
     size_t strLength = stringToSerial.size();
 
     // Check if the buffer has enough space to hold the string length and the string data
-    if (offset + sizeof(strLength) + strLength > bufferSize) {
+    if (offset + sizeof(strLength) + strLength > bufferSize)
+    {
         std::cerr << "Error: Buffer size is too small to serialize the string!" << std::endl;
         return;
     }
@@ -101,7 +130,8 @@ void Packet::deserialize(const char* buffer, size_t bufferSize, Packet* pkt)
 void Packet::deserializeString(size_t& offset, const char* buffer, size_t bufferSize, string* stringToDeser)
 {
     // Ensure the buffer is large enough to read the string length
-    if (offset + sizeof(size_t) > bufferSize) {
+    if (offset + sizeof(size_t) > bufferSize)
+    {
         std::cerr << "Error: Buffer size too small to read string length!" << std::endl;
         return;
     }
@@ -112,7 +142,8 @@ void Packet::deserializeString(size_t& offset, const char* buffer, size_t buffer
     offset += sizeof(strLength);
 
     // Check if the string length is reasonable and within the buffer limits
-    if (offset + strLength > bufferSize) {
+    if (offset + strLength > bufferSize)
+    {
         std::cerr << "Error: Buffer size too small to read string data!" << std::endl;
         return;
     }
@@ -123,16 +154,24 @@ void Packet::deserializeString(size_t& offset, const char* buffer, size_t buffer
 }
 
 // Calculate the total size required for serialization
-size_t Packet::getSerializedSize() const {
+size_t Packet::getSerializedSize() const
+{
     return id.size() + (sizeof(size_t) * 3) + usr.size() + message.size() ;
 }
 
-const string& Packet::getID() const {
+const string& Packet::getID() const
+{
     return id;
 }
 
-const string& Packet::getUsr() const {
+const string& Packet::getUsr() const
+{
     return usr;
+}
+
+std::string Packet::getMsg() const
+{
+    return message;
 }
 
 void Packet::setUsr(std::string inUSR)
@@ -148,12 +187,14 @@ string Packet::getComputerHostname()
     return host;
 }
 
-std::string Packet::getInternalIPAddress() {
+std::string Packet::getInternalIPAddress()
+{
     struct ifaddrs *ifaddr, *ifa;
     char ip[INET_ADDRSTRLEN];  // Buffer for the IP address (IPv4)
 
     // Get the list of network interfaces
-    if (getifaddrs(&ifaddr) == -1) {
+    if (getifaddrs(&ifaddr) == -1)
+    {
         perror("getifaddrs");
         return "";
     }
@@ -161,17 +202,20 @@ std::string Packet::getInternalIPAddress() {
     std::string internalIP;
 
     // Iterate through the linked list of interfaces
-    for (ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+    for (ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next)
+    {
         if (ifa->ifa_addr == nullptr) continue;
 
         // Check if the address is IPv4
-        if (ifa->ifa_addr->sa_family == AF_INET) {
+        if (ifa->ifa_addr->sa_family == AF_INET)
+        {
             // Convert the address to a readable format
             void* addr = &((struct sockaddr_in*)ifa->ifa_addr)->sin_addr;
             inet_ntop(AF_INET, addr, ip, sizeof(ip));
 
             // Skip loopback address
-            if (strcmp(ip, "127.0.0.1") != 0) {
+            if (strcmp(ip, "127.0.0.1") != 0)
+            {
                 internalIP = ip;  // Store the IP
                 break;  // Found the internal IP, exit the loop
             }
@@ -195,7 +239,8 @@ std::string Packet::md5(const std::string &str)
 
     std::stringstream ss;
 
-    for(unsigned char i : hash){
+    for(unsigned char i : hash)
+    {
         ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>( i );
     }
     return ss.str();
@@ -208,16 +253,20 @@ std::string Packet::generateUSERID()
     return md5(idBefore);
 }
 
-bool Packet::receiveAll(int clientSocket, char* buffer, size_t length) {
+bool Packet::receiveAll(int clientSocket, char* buffer, size_t length)
+{
     ssize_t total = 0;
     ssize_t n;
 
-    while (total < length) {
+    while (total < length)
+    {
         n = recv(clientSocket, buffer + total, length - total, 0);
-        if (n < 0) {
+        if (n < 0)
+        {
             std::cerr << "Error receiving data: " << strerror(errno) << '\n';
             return false; // Error occurred
-        } else if (n == 0) {
+        } else if (n == 0)
+        {
             std::cerr << "Connection closed by the server." << '\n';
             return false; // Connection closed
         }
@@ -244,26 +293,31 @@ bool Packet::checkAndReceivePacket(int* clientSocket, Packet* pkt)
     // Wait for the socket to be ready for reading
     int selectResult = select(*clientSocket + 1, &read_fds, NULL, NULL, &timeout);
 
-    if (selectResult < 0) {
+    if (selectResult < 0)
+    {
         std::cerr << "Error: select() failed! " << strerror(errno) << std::endl;
         delete[] buffer;  // Clean up
         return false;
-    } else if (selectResult == 0) {
+    } else if (selectResult == 0)
+    {
         // Timeout occurred, no data available
         delete[] buffer;  // Clean up
         return false; // No new packet has been sent
     }
 
     // Data is available to be read
-    if (FD_ISSET(*clientSocket, &read_fds)) {
+    if (FD_ISSET(*clientSocket, &read_fds))
+    {
         // Call receiveAll to receive the complete packet
-        if (Packet::receiveAll(*clientSocket, buffer, totalBytes)) {
+        if (Packet::receiveAll(*clientSocket, buffer, totalBytes))
+        {
             // Copy the data from the buffer to the object
             deserialize(buffer, totalBytes, pkt);
             std::cout << "Packet received successfully!" << '\n';
             delete[] buffer;  // Clean up
             return true;
-        } else {
+        } else
+        {
             std::cerr << "Failed to receive complete packet!" << '\n';
             delete[] buffer;  // Clean up
             return false;
@@ -282,7 +336,8 @@ int Packet::sendPacket(Packet pkt, int* clientSocket)
     serialize(buffer, bufferSize, &pkt);
 
     // Send the size of the packet first
-    if (send(*clientSocket, &bufferSize, sizeof(int), 0) == -1) {
+    if (send(*clientSocket, &bufferSize, sizeof(int), 0) == -1)
+    {
         delete[] buffer;  // Clean up memory in case of error
         return -1;
     }
@@ -294,12 +349,3 @@ int Packet::sendPacket(Packet pkt, int* clientSocket)
 
     return result; // Return the result of the send call
 }
-
-
-
-std::string Packet::getMsg() const {
-    return message;
-}
-
-
-
